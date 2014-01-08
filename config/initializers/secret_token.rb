ca44dc6dc06c62945c589b27e81af648a15a25cd
Adam Louis <adamlouis@gmail.com>
2014-01-08 13:12:26 -1000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TylerTest::Application.config.secret_key_base = '5b48e91ea6ae52a026099b4d93c61fcd40636c055dda201e2245a8263bb0a2073f5e483d5b05d4a71e1e996e6074b95cbc57090226485cc103cd649d60f70eef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TylerTest::Application.config.session_store :cookie_store, key: '_TylerTest_session'
